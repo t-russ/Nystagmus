ad88ec7baa4d14297d30a4ee3fcdbbe4fc9fecf3
Thomas Russell <tomrussell3567@gmail.com>
2024-08-10 16:35:26 +0100

    initial commit

---
+++ Plotting.py
@@ -0,0 +1,34 @@
+import plotly.graph_objects as go
+import ipywidgets as widgets
+from ipywidgets import interact, interact_manual
+import numpy as np
+import logging
+
+from EDFTrialParsing import EDFTrialParser
+
+logging.basicConfig(filename='logs\\std.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', filemode='w')
+logger = logging.getLogger(__name__)
+
+
+with open("C:\\Users\\tomru\Documents\\Sam Interning\\ad_5mn_whole_file.npy", 'rb') as f:
+    BREDFfileData = np.load(f, allow_pickle=True)
+
+
+brTrialParser = EDFTrialParser(BREDFfileData)
+brTrialParser.extractAllTrials()
+
+xRightData = brTrialParser.trials[1].sampleData['posXRight']
+yRightData = brTrialParser.trials[1].sampleData['posYRight']
+xLeftData = brTrialParser.trials[1].sampleData['posXLeft']
+yLeftData = brTrialParser.trials[1].sampleData['posYLeft']
+timeData = brTrialParser.trials[1].sampleData['time'] - (brTrialParser.trials[1].startTime)
+
+fig = go.FigureWidget()
+fig.add_trace(go.Scatter(x=timeData, y=xRightData, mode='lines', name='posXRight'))
+fig.add_trace(go.Scatter(x=timeData, y=yRightData, mode='lines', name='posYRight'))
+fig.add_trace(go.Scatter(x=timeData, y=xLeftData, mode='lines', name='posXLeft'))
+fig.add_trace(go.Scatter(x=timeData, y=yLeftData, mode='lines', name='posYLeft'))
+logger.info("Data Plotted")
+
+fig.show()
+
---
+++ notes.md
@@ -0,0 +1,13 @@
+Tasks:
+
+* [x]  -Scrub data, removing outliers
+        -Get index of recording start
+        -Get index of recording end
+        -Get eye used
+        -Extract each recording with relevant eyes
+
+* [ ] Plotting - interactivity
+
+    - [ ] Toggle for left and right
+    - [ ] Toggle for X and Y
+    - [ ] 
